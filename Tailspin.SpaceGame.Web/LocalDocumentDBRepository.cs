401bd63c944f08918d74fd9af211175a34754a06
SomeGreatdeveloper <75276685+SomeGreatdeveloper@users.noreply.github.com>
2021-06-14 14:46:07 +0200

    Change the page size

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -61,7 +61,7 @@ namespace TailSpin.SpaceGame.Web
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize) // take the number of items
+                .Take(pageSize -1) // take the number of items
                 .AsEnumerable(); // make enumeratable
 
             return Task<IEnumerable<T>>.FromResult(result);
